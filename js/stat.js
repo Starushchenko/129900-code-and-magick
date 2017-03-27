0bf5aa9c633095c8e6d7c28ba754f1fc18989ac7
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-03-28 00:06:56 +0300

    Разделение функций. Попытка упрощения функции рассчета максимального результата

--- js/stat.js
+++ js/stat.js
@@ -1,39 +1,50 @@
 'use strict';
-window.renderStatistics = function (ctx, names, times) {
+
+var drawACloud = function (ctx) {
   ctx.fillStyle = 'rgba(0,0,0,0.7)';
   ctx.fillRect(110, 20, 420, 270);
-
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
+};
 
+var drawAText = function (ctx) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
   ctx.fillText('Ура, вы победили!', 230, 30);
   ctx.fillText('Список результатов:', 222, 50);
+};
 
-  var maxTime = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > maxTime) {
-      maxTime = time;
-    }
-  }
+var calcMaxTime = function (times) {
+  return Math.max.apply(Math, times);
+};
 
+var randomOpacity = function (min, max) {
+  return Math.random() * (max - min) + min;
+};
+
+window.renderStatistics = function (ctx, names, times) {
   var histogramHeight = 150;
-  var step = histogramHeight / (maxTime - 0);
+  var step = histogramHeight / (calcMaxTime(times) - 0);
   var barWidth = 40;
   var indent = 50;
   var initialX = 210;
   var initialY = 100;
   var nameInitialY = 20;
-  for (var result = 0; result < times.length; result++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random() + ')';
-    }
+  var drawBarHistogram = function () {
     ctx.fillText(Math.round(times[i]), initialX + indent * i, histogramHeight - times[i] * step + 75);
     ctx.fillRect(initialX + indent * i, histogramHeight - times[i] * step + initialY, barWidth, times[i] * step);
     ctx.fillText(names[i], initialX + indent * i, histogramHeight + initialY + nameInitialY);
+  };
+
+  drawACloud(ctx);
+  drawAText(ctx);
+
+  for (var i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity(0.1, 1) + ')';
+    }
+    drawBarHistogram(ctx, names, times);
   }
 };
