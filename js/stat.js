b6c2db7261bd90b700a906509ad86e66e3cdc548
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2017-03-28 17:36:15 +0300

    Вынес функцию отрисовки столбика отдельно

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var drawACloud = function (ctx) {
+var drawCloud = function (ctx) {
   ctx.fillStyle = 'rgba(0,0,0,0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
 };
 
-var drawAText = function (ctx) {
+var drawText = function (ctx) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'black';
   ctx.fillText('Ура, вы победили!', 230, 30);
@@ -22,7 +22,7 @@ var randomOpacity = function (min, max) {
   return Math.random() * (max - min) + min;
 };
 
-window.renderStatistics = function (ctx, names, times) {
+var drawHistogramBar = function (ctx, names, times) {
   var histogramHeight = 150;
   var step = histogramHeight / (calcMaxTime(times) - 0);
   var barWidth = 40;
@@ -30,21 +30,24 @@ window.renderStatistics = function (ctx, names, times) {
   var initialX = 210;
   var initialY = 100;
   var nameInitialY = 20;
-  var drawBarHistogram = function () {
-    ctx.fillText(Math.round(times[i]), initialX + indent * i, histogramHeight - times[i] * step + 75);
-    ctx.fillRect(initialX + indent * i, histogramHeight - times[i] * step + initialY, barWidth, times[i] * step);
-    ctx.fillText(names[i], initialX + indent * i, histogramHeight + initialY + nameInitialY);
-  };
+  var i;
 
-  drawACloud(ctx);
-  drawAText(ctx);
+  if (names[i] === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity(0.1, 1) + ')';
+  }
+  ctx.fillText(Math.round(times[i]), initialX + indent * i, histogramHeight - times[i] * step + 75);
+  ctx.fillRect(initialX + indent * i, histogramHeight - times[i] * step + initialY, barWidth, times[i] * step);
+  ctx.fillText(names[i], initialX + indent * i, histogramHeight + initialY + nameInitialY);
+};
+
+window.renderStatistics = function (ctx, names, times) {
+
+  drawCloud(ctx);
+  drawText(ctx);
 
   for (var i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity(0.1, 1) + ')';
-    }
-    drawBarHistogram(ctx, names, times);
+    drawHistogramBar(ctx, names, times);
   }
 };
